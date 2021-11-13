3f40c5337a6ad58cf868d4b5b9c427638927fe18
Matteo Ferron <sviluppo@matteoferron.it>
2021-11-13 10:19:33 +0100

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<header class="text-center bg-blue-800 text-white p-4 mb-10">
+  <div class="text-3xl md:text-5xl font-bold mb-3">
+    <i class="fas fa-viruses"></i>
+      Tracciamento Casi Covid-19
+    </div>
+  <p>
+    API By
+    <a href="https://covid19api.com" class="text-blue-300" target="_blank">covid19api.com</a>
+  </p>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
